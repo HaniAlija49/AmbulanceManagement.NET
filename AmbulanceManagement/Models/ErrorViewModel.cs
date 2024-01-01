eb2fc9d19fcdde4bb62c44ec2c72659bfd4443a7
Hani Alija <ha30049@seeu.edu.mk>
2024-01-01 19:57:20 +0100

    Add project files.

---
+++ AmbulanceManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AmbulanceManagement.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
