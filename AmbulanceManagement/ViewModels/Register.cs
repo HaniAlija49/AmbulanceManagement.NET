41408a310033b16626634ae65ce0772530d4fdd5
Hani Alija <ha30049@seeu.edu.mk>
2024-01-03 12:35:02 +0100

    Partial-LogIn/out

---
+++ AmbulanceManagement/ViewModels/Register.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AmbulanceManagement.ViewModels
+{
+    public class Register
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100,ErrorMessage = " the {0} must be at least {2} character long.", MinimumLength = 6)]
+
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set;}
+
+        [Required]
+        [Display(Name ="RoleName Name")]
+
+        public string RoleName { get; set; }
+    }
+}
