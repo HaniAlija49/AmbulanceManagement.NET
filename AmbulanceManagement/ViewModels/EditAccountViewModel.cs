7f2743313f2799cb183d8f783b2910643a655e33
Hani Alija <ha30049@seeu.edu.mk>
2024-01-03 16:02:21 +0100

    Crud - Userave

---
+++ AmbulanceManagement/ViewModels/EditAccountViewModel.cs
@@ -0,0 +1,29 @@
+﻿using AmbulanceManagement.Utility;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AmbulanceManagement.ViewModels
+{
+	public class EditAccountViewModel
+	{
+			public string Id { get; set; }
+
+			[Required(ErrorMessage = "Name is required.")]
+			public string Name { get; set; }
+
+			public int Number { get; set; }
+
+			[DataType(DataType.Date)]
+			public DateTime DateOfBirth { get; set; }
+
+			public Gender Gender { get; set; }
+
+			public string Education { get; set; }
+
+			public string Type { get; set; }
+
+			[AllowNull]
+			public string? Biography { get; set; }
+		}
+
+	}
