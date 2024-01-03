a635535a8e538347f27bb89804048cf2e7ad9c0b
UvejsMurtezi <um30025@seeu.edu.mk>
2024-01-03 12:38:11 +0100

    Login & Register Views and Account Controller, ApplicationUser

---
+++ AmbulanceManagement/Controllers/AccountController.cs
@@ -0,0 +1,107 @@
+﻿using AmbulanceManagement.Data;
+using AmbulanceManagement.ViewModels;
+using AmbulanceManagement.Data;
+using AmbulanceManagement.Models;
+using AmbulanceManagement.Utility;
+using AmbulanceManagement.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace InventoryManagement.Controllers
+{
+	public class AccountController : Controller
+	{
+
+		private ApplicationDbContext _dbContext;
+		UserManager<ApplicationUser> _userManager;
+		SignInManager<ApplicationUser> _signInManager;
+		RoleManager<IdentityRole> _roleManager;
+
+		public AccountController(ApplicationDbContext dbContext, UserManager<ApplicationUser> userManager,
+		SignInManager<ApplicationUser> signInManager,
+		RoleManager<IdentityRole> roleManager)
+		{
+			_dbContext = dbContext;
+			_userManager = userManager;
+			_signInManager = signInManager;
+			_roleManager = roleManager;
+		}
+
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Login([FromForm] LoginViewModel loginViewModel)
+		{
+			if (!ModelState.IsValid)
+				return View(loginViewModel);
+
+			var result = await _signInManager.PasswordSignInAsync(loginViewModel.Email, loginViewModel.Password, loginViewModel.RememberMe, false);
+
+			if (result.Succeeded)
+			{
+				return RedirectToAction("Index", "Home");
+			}
+
+			ModelState.AddModelError("", "Invalid Login");
+
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> LogOff()
+		{
+			await _signInManager.SignOutAsync();
+			return RedirectToAction("Login", "Account");
+		}
+
+
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterViewModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View();
+
+			}
+
+			if (!_roleManager.RoleExistsAsync(Helper.Admin).GetAwaiter().GetResult())
+			{
+				await _roleManager.CreateAsync(new IdentityRole(Helper.Admin));
+				await _roleManager.CreateAsync(new IdentityRole(Helper.User));
+			}
+
+			var user = new ApplicationUser()
+			{
+				UserName = model.Email,
+				Email = model.Email,
+				Name = model.Name
+			};
+
+			var result = await _userManager.CreateAsync(user, model.Password);
+
+			if (result.Succeeded)
+			{
+				await _userManager.AddToRoleAsync(user, model.RoleName);
+				await _signInManager.SignInAsync(user, isPersistent: false);
+				RedirectToAction("Index", "Home");
+			}
+			foreach (var error in result.Errors)
+			{
+				ModelState.AddModelError("", error.Description);
+			}
+
+			return View(model);
+		}
+	}
+}
