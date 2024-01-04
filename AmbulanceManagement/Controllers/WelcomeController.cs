ff43863b811db82056e0caadc29e16f5766590c2
Hani Alija <ha30049@seeu.edu.mk>
2024-01-04 15:34:15 +0100

    Design

---
+++ AmbulanceManagement/Controllers/WelcomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AmbulanceManagement.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
