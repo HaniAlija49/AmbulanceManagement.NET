37f4297b8161dbbceeee1661f135b8b4fd88f23f
Jasin30373 <ji30373@seeu.edu.mk>
2024-01-05 12:52:34 +0100

    idk

---
+++ AmbulanceManagement/Views/ReportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AmbulanceManagement.Views
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
